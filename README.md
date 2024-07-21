3ebcd328502dda9dd57fb479195255e48462ff3d
Lukasz Michalik <l.michalik004@gmail.com>
2024-07-21 17:29:41 +0200

    add supported ligues

---
+++ README.md
@@ -0,0 +1,1 @@
+add ligues and teams that will be tracked
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export const fixtureName = ['Goals Over/Under', 'Total - Home', 'Total - Away'];
 
-export const fixtureValues = [0.5, 1.5, 2.5, 3.5, 4.5]; 
+export const fixtureValues = [0.5, 1.5, 2.5, 3.5, 4.5];
+
+export const supportedLigues = [39, 307, 218, 144, 344, 71, 172, 265, 170, 170, 119, 233, 329, 244, 61, 197, 140, 88, 119, 164, 239, 239, 362, 365, 262, 78, 103, 304, 281, 106, 94, 283, 286, 332, 179, 207, 113, 268, 253, 271, 299, 135];
