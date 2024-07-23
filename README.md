8a43985cc255aa435d0f8b9a04fee65d701f4f5c
Lukasz Michalik <l.michalik004@gmail.com>
2024-07-23 23:54:28 +0200

    work with bets

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-// check all bets from 365 bets
 // stats and connect it to the match
 // start writing logic that will handle all cases
+// add flag for match that will inform if match stats are fetched
--- src/types/index.ts
+++ src/types/index.ts
@@ -9,3 +9,68 @@ export interface IOptions {
   params: any;
   path: string
 }
+
+export interface IGoals {
+  '0.5': number;
+  '1.5': number;
+  '2.5': number;
+  '3.5': number;
+  '4.5': number;
+  '5.5': number;
+}
+
+export interface IHandicap {
+  '-3': number;
+  '-2': number;
+  '-1': number;
+  '+1': number;
+  '+2': number;
+  '+3': number;
+}
+
+export interface IBet {
+  'Wynik meczu (z wyłączeniem dogrywki)': {
+    'home': number;
+    'draw': number;
+    'away': number;
+  },
+  'Podwójna szansa': {
+    '1X': number;
+    '12': number;
+    'X2': number;
+  },
+  'Gole Powyżej/Poniżej': {
+    'Powyżej': IGoals,
+    'Poniżej': IGoals
+  },
+  'Oba zespoły strzelą gola': {
+    'Tak': number;
+    'Nie': number;
+  },
+  'Gole gospodarzy powyżej/poniżej': {
+    'Powyżej': IGoals,
+    'Poniżej': IGoals
+  },
+  'Gole gości powyżej/poniżej': {
+    'Powyżej': IGoals,
+    'Poniżej': IGoals
+  },
+  'Handicap': {
+    'home': IHandicap,
+    'away': IHandicap
+  },
+  'Pierwszy gol': {
+    'home': number;
+    'without': number;
+    'away': number;
+  },
+  'Wynik 1. połowy': {
+    'home': number;
+    'draw': number;
+    'away': number;
+  },
+  '1. połowa, gole powyżej/poniżej': {
+    'Powyżej': IGoals,
+    'Poniżej': IGoals
+  }
+}
